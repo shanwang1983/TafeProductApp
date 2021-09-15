fbbeaa5a846496d7b4c77f567ff0096b4af39dd1
Shan Wang <shan.wang3@student.tafesa.edu.au>
2021-09-15 10:03:48 +0930

    Added the delivery charge $25

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -34,6 +34,8 @@ namespace ProductApps
                 cProduct = new Product(Convert.ToDecimal(priceTextBox.Text), Convert.ToInt16(quantityTextBox.Text));
                 cProduct.calTotalPayment();
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
+                //modify by shan wang to add toatal charge calculation.
+                totalChargeTextBlock.Text = Convert.ToString(cProduct.TotalPayment + 25);
             }
             catch (FormatException)
             {
